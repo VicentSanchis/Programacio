c99f2bce62bb240c81cfb8fa1a117d98c8ccf696
VicentSanchis <vsanchis.inf@gmail.com>
2024-01-09 11:53:25 +0100

    Solucions

--- js/mathjax.js
+++ js/mathjax.js
@@ -1,14 +1,21 @@
 window.MathJax = {
-    tex: {
-      inlineMath: [["\\(", "\\)"]],
-      displayMath: [["\\[", "\\]"]],
-      processEscapes: true,
-      processEnvironments: true
-    },
-    options: {
-      ignoreHtmlClass: ".*|",
-      processHtmlClass: "arithmatex"
-    }
-  };
+  tex: {
+    inlineMath: [["\\(", "\\)"]],
+    displayMath: [["\\[", "\\]"]],
+    processEscapes: true,
+    processEnvironments: true
+  },
+  options: {
+    ignoreHtmlClass: ".*|",
+    processHtmlClass: "arithmatex"
+  }
+};
+
+document$.subscribe(() => { 
+
+
+  MathJax.typesetPromise()
+})
+
   
   
